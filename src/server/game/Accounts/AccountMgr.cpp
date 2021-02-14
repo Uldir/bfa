096fdbd822f0b46dc139941f4afa49c40916996c
FuzionCore\fuzion <mefi.dark@gmai.com>
2021-02-15 01:41:16 +0200

    update

--- src/server/game/Accounts/AccountMgr.cpp
+++ src/server/game/Accounts/AccountMgr.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2020 LatinCoreTeam
- * Copyright (C) Thordekk
+ * Copyright (C) 2020 FuzionCore Project
+ * Copyright (C) Thordekk and Frozen
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
